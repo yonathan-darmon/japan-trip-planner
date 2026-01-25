28fa6781383d9c22f2b03af85845983d2aa28a45
yonathan <yonathan.darmon@laplateforme.io>
2026-01-25 22:15:28 +0100

    Update API URL for production

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://api.monsupersite.com/api' // Placeholder for production URL
+    apiUrl: 'https://japan-trip-planner-backend-iawy.onrender.com/api' // Placeholder for production URL
 };
