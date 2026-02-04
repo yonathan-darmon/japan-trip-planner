dbd44d04b168af93c17bdac60331009ba628e647
yonathan <yonathan.darmon@laplateforme.io>
2026-02-04 20:24:55 +0100

    chore(env): update production appUrl

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     apiUrl: 'https://japan-trip-planner-backend-iawy.onrender.com/api',
     wsUrl: 'https://japan-trip-planner-backend-iawy.onrender.com',
-    appUrl: 'https://japan-trip-planner-backend-iawy.onrender.com'
+    appUrl: 'https://japan-trip-planner-mqxq.onrender.com'
 };
